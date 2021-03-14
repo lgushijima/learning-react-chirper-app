06863ff92607b01e33123cedd63df10fd26fc0d6
Gustavo Ushjima <gustavo@inno.com.br>
2021-03-14 14:13:30 -0300

    added Tweet component and toggle like actions calls

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import {getInitialData} from '../utils/api'
 import {receiveTweets} from '../actions/tweets'
 import {receiveUsers} from '../actions/users'
 import {setAuthedUser} from '../actions/authedUser'
+import {showLoading, hideLoading} from "react-redux-loading";
 
 const AUTHED_ID = 'thlermcginnis'
 
 export function handleInitialData () {
     return (dispatch) =>{
+        dispatch(showLoading())
         return getInitialData()
         .then(({users,tweets}) =>{
             dispatch(receiveTweets(tweets))
             dispatch(receiveUsers(users))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import tweets from "./tweets";
 import users from "./user";
 import authedUser from "./authedUser";
+import {loadingBarReducer} from "react-redux-loading";
 
-export default combineReducers({ authedUser, users, tweets });
+export default combineReducers({ authedUser, users, tweets, loadingBar :loadingBarReducer });
